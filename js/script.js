4ad9717d0f90d62c4cf33b30cb09fe5f3dc39d02
SISISISISI1 <71912455+SISISISISI1@users.noreply.github.com>
2020-09-25 20:25:07 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* Made by Rob Diaz
+
+For more animations visit https://www.robdiaz.com/
+Contact: hello@robdiaz.com
+
+*/
+
+var animData = {
+        container: document.getElementById('container'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        path: 'https://raw.githubusercontent.com/robbdiazz/uianims/master/toggledayandnight.json'
+    };
+    var anim = bodymovin.loadAnimation(animData);
